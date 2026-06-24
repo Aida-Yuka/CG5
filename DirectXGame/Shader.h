b7f4ba597fad3ecac5d757159bfa28a899b3b73a
Aida_Yuka <K024G0052@g.neec.ac.jp>
2026-06-24 13:14:45 +0900

    00-04 バグ直し中

--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include<string>
-#include<d3d12.h>
+#include<d3dcompiler.h>
+#include <dxcapi.h>
 
 class Shader{
 public:
 	//シェーダーファイルを読み込み、コンパイル済みデータを生成する
 	void Load(const std::wstring& filePath, const std::wstring& shaderModel);
+	void LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel);
 
 	//生成したコンパイル済みデータを取得する
 	ID3DBlob* GetBlob();
+	IDxcBlob* GetDxcBlob();
 
 	//コンストラクタ
 	Shader();
@@ -18,4 +21,5 @@ public:
 
 private:
 	ID3DBlob* blob_ = nullptr;//コンストラクタで初期化しなくていい
+	IDxcBlob* dxcBlob_ = nullptr;
 };
