09a469b5ddca4c01d89babfa50485149264f59f9
Aida_Yuka <K024G0052@g.neec.ac.jp>
2026-07-27 17:50:54 +0900

    00-08

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,11 +1,17 @@
+#include "Test.hlsli"
+
 struct PixelShaderOutput
 {
 	float4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main()
+PixelShaderOutput main(VertexShaderOutput input)
 {
 	PixelShaderOutput output;
-	output.color = float4(1.0f, 1.0f, 1.0f, 1.0f);
+	float2 uv = input.texcoord;
+
+	//位置セット(x y z w)か、カラーセット(r g b a)でアクセス出来る
+	output.color = float4(uv.x, uv.y, 0.0f, 0.0f);
+
 	return output;
 }
